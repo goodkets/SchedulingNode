f7e849861cd27877c88fca0730c99ae7e0f323fc
goodkat <2066442690@qq.com>
2025-05-17 23:21:59 +0800

    登录接口添加

--- db/index.js
+++ db/index.js
@@ -1,13 +1,16 @@
-// 导入 mysql 模块
-const mysql = require('mysql')
+// 导入 mysql2 模块
+const mysql = require('mysql2/promise');
 
-// 创建数据库连接对象
+// 创建数据库连接池
 const db = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: 'root',
-  database: 'matchmaking',
-})
+  password: 'www..com',
+  database: 'schedulingfront',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 // 向外共享 db 数据库连接对象
-module.exports = db
+module.exports = db;
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require("express")
-const app = express()
-const prot = 8889
-const router = require('./routes/index')
-app.use(express.json())
-app.use(router)
-app.listen(prot, () => {
-    console.log(`Example app listening on prot http://127.0.0.1:${prot}`)
+const express = require("express") 
+const app = express() 
+const ip = '127.0.0.1' 
+const port = 8889 
+const router = require('./routes/index') 
+const cors = require('cors'); // 引入 cors 中间件
+
+// 使用 cors 中间件
+app.use(cors({
+  origin: 'http://localhost:9527', // 允许的前端域名
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true // 允许携带凭证
+}));
+
+app.use(express.json()) 
+app.use('/api', router) 
+app.listen(port,ip, () => { 
+    console.log(`Example app listening on http://${ip}:${port}`) 
 })
